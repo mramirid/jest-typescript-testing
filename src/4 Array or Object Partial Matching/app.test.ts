fb0e11370ecc960805f06ae1df28a4758ba5da82
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2021-01-22 23:27:40 +0700

    ✅ Simple object partial matching

---
+++ src/4 Array or Object Partial Matching/app.test.ts
@@ -0,0 +1,31 @@
+describe("Array/Object partial match with objectContaining and arrayContaining", () => {
+  describe("Simple object partial matching", () => {
+    test("id should match", () => {
+      const obj = {
+        id: "111",
+        productName: "Jest Handbook",
+        url: "https://jesthandbook.com",
+      };
+      expect(obj).toEqual(
+        expect.objectContaining({
+          id: "111",
+        })
+      );
+    });
+
+    it("Should have right id and name", () => {
+      const user = {
+        id: 1,
+        friends: [],
+        name: "Hugo",
+        url: "https://codewithhugo.com",
+      };
+      expect(user).toEqual(
+        expect.objectContaining({
+          id: 1,
+          name: "Hugo",
+        })
+      );
+    });
+  });
+});
